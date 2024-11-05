b2506cc4878eccf3a749500c54b26fd70751223c
unknown <altohatolegenov389@gmail.com>
2024-11-05 17:43:44 +0500

    tovar

--- src/pages/products/model/Product.ts
+++ src/pages/products/model/Product.ts
@@ -4,9 +4,9 @@ export interface ProductModel {
   price: number;
 }
 export const products: ProductModel[] = [
-  { id: 0, name: "Banana", price:500},
-  { id: 1, name: "Peach" , price:700 },
-  { id: 2, name: "Apple", price:600 },
-  { id: 3, name: "Qiwi", price:100 },
-  { id: 4, name: "Pineapple", price:1500 },
+  { id: 0, name: "Banana", price: 500 },
+  { id: 1, name: "Peach", price: 700 },
+  { id: 2, name: "Apple", price: 600 },
+  { id: 3, name: "Qiwi", price: 100 },
+  { id: 4, name: "Pineapple", price: 1500 },
 ];
